22339b639c4eaf4ae584c56abfbad765e2c7d3d6
Souluigi <marcos2003mlr@gmail.com>
2025-10-18 17:53:48 -0300

    feat(repository): Cria CommerceAddRepository

---
+++ lib/data/repositories/commerce_add_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/commerce_add_model.dart';
+import '../model/commerce_model.dart';
+
+class CommerceAddRepository{
+  final String _baseUrl = 'http://192.168.0.104:8080';
+
+  Future<Map<String, dynamic>> sendCreationRequest(CommerceAddModel data) async{
+    final uri = Uri.parse('$_baseUrl/comercios/add');
+    final response = await http.post(
+      uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data.toJson()),
+    );
+    if(response.statusCode == 201 || response.statusCode == 200){
+      return jsonDecode(response.body);
+    }else{
+      final errorBody = jsonDecode(response.body);
+      throw Exception('Erro de API. Status: ${response.statusCode}. Mensagem: ${errorBody['message']}');
+    }
+  }
+}
