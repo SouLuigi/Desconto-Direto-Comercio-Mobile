e06fc9500d3e33c3aab7d97fcbe179ac0ddc1ca6
Souluigi <marcos2003mlr@gmail.com>
2025-10-23 20:59:26 -0300

    style(theme): Cria o arquivo colors.dart para padronização de cores

---
+++ lib/ui/core/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract final class AppColors{
+  static const Blue1 = Color(0xFF023047);
+  static const Yellow1 = Color(0xFFFFB703);
+  static const White1 = Color(0xFFFFFFFF);
+  static const Black1 = Color(0xFF000000);
+  static const Red1 = Color(0xFFBC2121);
+}
