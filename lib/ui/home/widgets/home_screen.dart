6843c789cb4e41eaa0f477592ed1716516bd6863
Souluigi <marcos2003mlr@gmail.com>
2025-11-15 17:17:41 -0300

    feat(home): Cria a estrutura inicial da HomeScreen

--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -1,14 +1,47 @@
+import 'package:desconto_direto_comercio_mobile/ui/core/themes/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold()
-
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.Blue1,
+          leading: IconButton(
+            icon: Icon(Icons.account_circle_outlined, color: AppColors.Yellow1),
+            onPressed: () {},
+            tooltip: 'Perfil',
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 47,
+              color: AppColors.Yellow1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Minhas Ofertas',
+                  style: GoogleFonts.kaiseiDecol(
+                    textStyle: const TextStyle(
+                      color: AppColors.White1,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
