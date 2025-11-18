5a2e9ec3d7917fa88cb076e107b86f9b40264add
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-11-18 20:40:29 -0300

    refactor: trocando id de int por string no data

--- lib/data/services/commerce_service.dart
+++ lib/data/services/commerce_service.dart
@@ -55,7 +55,7 @@ class CommerceService {
     }
   }
 
-  Future<Commerce> getCommerceById(int id) async {
+  Future<Commerce> getCommerceById(String id) async {
     try {
       return await _repository.getById(id);
     } catch (e) {
@@ -63,7 +63,7 @@ class CommerceService {
     }
   }
 
-  Future<Offer?> getOffersByIdCommerce(int id) async {
+  Future<Offer?> getOffersByIdCommerce(String id) async {
     try {
       final jsonResponse = await _repository.getById(id);
       return jsonResponse.offer;
@@ -72,7 +72,7 @@ class CommerceService {
     }
   }
 
-  Future<Flyer?> getFlyersByIdCommerce(int id) async {
+  Future<Flyer?> getFlyersByIdCommerce(String id) async {
     try {
       final jsonResponse = await _repository.getById(id);
       return jsonResponse.flyer;
@@ -92,7 +92,7 @@ class CommerceService {
     }
   }
 
-  Future<void> deleteCommerce(int id) async {
+  Future<void> deleteCommerce(String id) async {
     try {
       return await _repository.delete(id);
     } catch (e) {
@@ -100,7 +100,7 @@ class CommerceService {
     }
   }
 
-  Future<bool> uploadImageOfCommerce(int id, File image) async {
+  Future<bool> uploadImageOfCommerce(String id, File image) async {
     try {
       await _repository.uploadImage(id, image);
       return true;
